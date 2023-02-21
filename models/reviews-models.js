e3dc8583b1a3fb81ec7ca1984039d13a4d5e80f7
CryptoSheikh <tanzil@CryptoSeikhsMBP.lan>
2023-02-21 16:48:22 +0000

    remove comments

--- models/reviews-models.js
+++ models/reviews-models.js
@@ -1,9 +1,6 @@
 const db = require("../db/connection");
 
-//comment_count which is the total count of all the comments with this review_id - you should make use of queries to the database in order to achieve this.
-
 selectReviews = () => {
-	//query should return all from reviews and the comment count for each review
 	return db
 		.query(
 			`SELECT reviews.*, COUNT(comments.review_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`
