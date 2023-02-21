116478f931a6f969f0579122bb12e337ae996201
CryptoSheikh <tanzil@CryptoSeikhsMBP.lan>
2023-02-21 11:27:00 +0000

    Removed unneeded express usage

--- db/app.js
+++ db/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const apiRouter = require("../routers/api-router");
 
-app.use(express.json());
 app.use("/api", apiRouter);
 
 module.exports = app;
