a4ca76a50e7391399db59233251f67815cbad41c
tanzil-eth <120569290+tanzil-eth@users.noreply.github.com>
2023-02-24 14:50:07 +0000

    Complete comments endpoint (#4)

--- routers/reviews-router.js
+++ routers/reviews-router.js
@@ -2,10 +2,12 @@ const express = require("express");
 const {
 	getReviews,
 	getReviewById,
+	getCommentsByReviewId,
 } = require("../controllers/reviews-controller");
 const reviewsRouter = express.Router();
 
 reviewsRouter.route("/").get(getReviews);
 reviewsRouter.route("/:review_id").get(getReviewById);
+reviewsRouter.route("/:review_id/comments").get(getCommentsByReviewId);
 
 module.exports = reviewsRouter;
